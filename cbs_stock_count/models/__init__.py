bcf85f58b5b0c56c940b48f74a2ef386e25ccce2
ali-cbs <ali@cbs.jo>
2025-12-09 16:01:05 +0300

    Add files via upload
    cbs_stock_count

---
+++ cbs_stock_count/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import product_category
+from . import stock_count_line
+from . import stock_count_session
---
+++ cbs_stock_count/wizard/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import stock_count_refuse_wizard
